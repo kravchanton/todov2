cc3f3c921327a24cadf37b0896e4ff4f2177e546
Kravchuk Anton <Kravch-anton@mail.ru>
2023-02-23 23:36:57 +0300

    fix style ul

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,7 +2,9 @@ import React, {useState, KeyboardEvent, ChangeEvent} from 'react';
 import {filterValuesType} from "./App";
 import {AddItemForm} from "./AddItemForm";
 import {EditableSpan} from "./EditableSpan";
-import { Button } from '@mui/material';
+import {Button, Checkbox, IconButton} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 
 export type TaskType = {
     id: string
@@ -26,17 +28,19 @@ type TodoListPropsType = {
 }
 
 export const TodoList = (props: TodoListPropsType) => {
-let addTask = (title: string) => {
-    props.addTask(title, props.id)
-}
-let changeTodoListTitle= (title: string) => {
-    props.changeTodoListTitle(title, props.id)
-}
+    let addTask = (title: string) => {
+        props.addTask(title, props.id)
+    }
+    let changeTodoListTitle = (title: string) => {
+        props.changeTodoListTitle(title, props.id)
+    }
 
     return (
         <div>
-            <h3><EditableSpan title={props.title} changeTitle={changeTodoListTitle} />
-                <Button onClick={() => props.removeTodoList(props.id)}>✖</Button>
+            <h3><EditableSpan title={props.title} changeTitle={changeTodoListTitle}/>
+                <IconButton aria-label="delete" onClick={() => props.removeTodoList(props.id)}>
+                    <DeleteIcon/>
+                </IconButton>
             </h3>
 
 
@@ -53,6 +57,7 @@ let changeTodoListTitle= (title: string) => {
                     }
                     return (
                         <li key={t.id} className={t.isDone === true ? 'is-done' : ''}>
+
                             <input type="checkbox" checked={t.isDone} onChange={changeTaskStatus}/>
                             <EditableSpan title={props.title} changeTitle={changeTaskTitle}/>
                             <Button onClick={() => props.removeTask(t.id, props.id)}>✖</Button>
@@ -62,14 +67,19 @@ let changeTodoListTitle= (title: string) => {
             </ul>
             <div>
                 <Button
+                    size="small"
                     variant={props.filter === 'all' ? "contained" : "outlined"}
-                        onClick={() => props.changeFilter('all', props.id)}>All
+                    onClick={() => props.changeFilter('all', props.id)}>All
                 </Button>
-                <Button variant={props.filter === 'active' ? "contained" : "outlined"}
-                        onClick={() => props.changeFilter('active', props.id)}>Active
+                <Button
+                    size="small"
+                    variant={props.filter === 'active' ? "contained" : "outlined"}
+                    onClick={() => props.changeFilter('active', props.id)}>Active
                 </Button>
-                <Button variant={props.filter === 'completed' ? "contained" : "outlined"}
-                        onClick={() => props.changeFilter('completed', props.id)}>Completed
+                <Button
+                    size="small"
+                    variant={props.filter === 'completed' ? "contained" : "outlined"}
+                    onClick={() => props.changeFilter('completed', props.id)}>Completed
                 </Button>
             </div>
         </div>
